8b59b57f3944bf37ff2af80dd947b244486570ba
jueluddin <juddin@panopen.com>
2017-03-29 22:12:14 -0400

    added custom styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice]= "Article was successfully created"
+      flash[:success]= "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -31,7 +31,7 @@ class ArticlesController < ApplicationController
   def update
     if @article.update(article_params)
 
-      flash[:notice] = "Article was successfully updated"
+      flash[:success] = "Article was successfully updated"
 
       redirect_to article_path(@article)
 
@@ -47,7 +47,7 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     redirect_to articles_path
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
   end
 
 private
